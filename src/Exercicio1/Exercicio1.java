92d78ae546fc98e8fdf39fd5eb41f3d4123a75d1
Diego Garcia <A22DiegoGP@iessanclemente.net>
2022-11-25 01:25:36 +0100

    a

--- src/Exercicio1/Exercicio1.java
+++ src/Exercicio1/Exercicio1.java
@@ -11,23 +11,28 @@ public class Exercicio1 {
         double max = 0;
         double min = 0;
         int introducidos = -1;
+        boolean condicion = true;
         do {
             lectura = sc.nextLine().trim().replace(",", ".");
-            num = Double.parseDouble(lectura);
-            suma = suma + num;
-            ++introducidos;
-            if (num > max) {
-                max = num;
-            }
-            if (min > num) {
-                min = num;
-            }
+           if (lectura.equals("00") && condicion == true) {
+                System.out.println("No hay datos");
+           } else if(lectura.equals("00") && condicion == false){
+                System.out.printf("Min: %.2f%n", min);
+                System.out.printf("Max: %.2f%n", max);
+                System.out.printf("Suma: %.2f%n", suma);
+                System.out.printf("Media: %.2f%n", (suma / (introducidos+1)));
+           } else {
+                num = Double.parseDouble(lectura);
+                suma = suma + num;
+                ++introducidos;
+                if (num > max) {
+                    max = num;
+                }
+                if (min > num) {
+                    min = num;
+                }
+           }
+           condicion = false;
         } while (!lectura.equals("00"));
-        if (lectura.equals("00")) {
-            System.out.printf("Min: %.2f%n", min);
-            System.out.printf("Max: %.2f%n", max);
-            System.out.printf("Suma: %.2f%n", suma);
-            System.out.printf("Media: %.2f%n", (suma / introducidos));
-        }
     }
 }
--- src/Exercicio5/exercicio5.java
+++ src/Exercicio5/exercicio5.java
@@ -6,19 +6,61 @@ public class exercicio5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int inicio = 0;
-        int numinicio =0;
+        int numinicio = 0;
         int numdestino = 0;
         int pisos = 0;
-        boolean sair = false;
+        int valoranterior = 0;
+        boolean sair1 = false;
+        boolean sair2 = false;
+        boolean condicion = true;
         do {
             inicio = sc.nextInt();
+            condicion = true;
+            sair2 = false;
+            pisos = 0;
             if (inicio >= 0) {
                 do {
-                    
-                } while (numinicio >= 0);
+                    numinicio = sc.nextInt();
+                    if (numinicio >= 0) {
+                        numdestino = sc.nextInt();
+                        if (numdestino >= 0) {
+                            if (condicion == true) {
+                                int op1 = numinicio - inicio;
+                                int op1ab = Math.abs(op1);
+                                pisos = pisos + op1ab;
+                                int op2 = numdestino - numinicio;
+                                int op2ab = Math.abs(op2);
+                                pisos = pisos + op2ab;
+                                valoranterior = numdestino;
+                                condicion = false;
+                            } else {
+                                int op3 = valoranterior - numinicio;
+                                int op3ab = Math.abs(op3);
+                                int op4 = numdestino - numinicio;
+                                int op4ab = Math.abs(op4);
+                                pisos = pisos + op3ab + op4ab;
+                                valoranterior = numdestino;
+                            }
+                        } else {
+                            sair2 = true;
+                            int op5 = inicio - numinicio;
+                            int op5ab = Math.abs(op5);
+                            System.out.println(op5ab);
+                        }
+
+                    } else {
+                        sair2 = true;
+                        if (pisos == 0) {
+                            System.out.println(0);
+                        } else {
+                            System.out.println(pisos);
+                        }
+
+                    }
+                } while (sair2 != true);
             } else {
-                sair = true;
+                sair1 = true;
             }
-        } while (sair != true);
+        } while (sair1 != true);
     }
 }
